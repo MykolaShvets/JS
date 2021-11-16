46a3209bc332cf73e93dcde516ffca4a86d030cd
MykolaShvets <m2009sv@gmail.com>
2021-11-16 12:58:00 +0200

    hw

--- homework3/main.js
+++ homework3/main.js
@@ -153,7 +153,7 @@
 // - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
 
 // for (i = 0; i < 100; i++){
-//     if(i % 2 === 1){
+//     if(i % 2 > 0){
 //     console.log(`${i}`);
 //     document.write(`<p> ${i} </p>`);
 //     }
