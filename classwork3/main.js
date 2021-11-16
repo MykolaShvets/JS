5be2e025b1b937f755f884b7167c7fd4f54964c5
MykolaShvets <m2009sv@gmail.com>
2021-11-16 15:02:29 +0200

    classwork3

---
+++ classwork3/main.js
@@ -0,0 +1,124 @@
+// зробити масив з 10 чисел [2,17,13,6,22,31,45,66,100,-18]та:
+
+let num =  [2,17,13,6,22,31,45,66,100,-18];
+
+// 1. перебрати його циклом while
+
+// let i = 0;
+// while(i < num.length){
+//     console.log(num[i]);
+//     i++;
+// }
+
+// 2. перебрати його циклом for
+
+// for(let i = 0; i < num.length; i++){
+//     console.log(num[i]);
+// }
+
+
+
+// 3. перебрати циклом while та вивести  числа тільки з непарним індексом
+
+// let i = 0;
+// while (i < num.length){
+//     if (i % 2 > 0){
+//         console.log(num[i]);
+//     }
+//     i++;
+// }
+
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+
+// for(let i = 0; i < num.length; i++){
+//     if (i % 2 > 0){
+//         console.log(num[i]);
+//     }
+// }
+
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
+
+// let i = 0;
+// while (i < num.length){
+//     if (i % 2 === 0){
+//         console.log(num[i]);
+//     }
+//     i++;
+// }
+
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
+
+// for(let i = 0; i < num.length; i++){
+//     if (i % 2 === 0){
+//         console.log(num[i]);
+//     }
+// }
+
+// 7. замінити кожне число кратне 3 на слово "okten"
+
+// for(let i = 0; i < num.length; i++){
+//     if (i % 3 === 0){
+//         num[i] = 'okten';
+//     }
+// }
+// console.log(num);
+
+// 8. вивести масив в зворотньому порядку.
+
+// for (let i = num.length - 1; i >= 0; i--){
+//     console.log(num[i]);
+// }
+
+// 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
+//1.
+// let i = num.length - 1;
+// while(i >= 0){
+//     console.log(num[i]);
+//     i--;
+// }
+
+//2.
+// for (let i = num.length - 1; i >= 0; i--){
+//     console.log(num[i]);
+// }
+
+//3.
+// let i = num.length-1;
+// while (i >= 0){
+//     if (i % 2 > 0){
+//         console.log(num[i]);
+//     }
+//     i--;
+// }
+
+//4.
+// for(let i = num.length - 1; i >= 0; i--){
+//     if (i % 2 > 0){
+//         console.log(num[i]);
+//     }
+// }
+
+//5.
+// let i = num.length - 1;
+// while (i >= 0){
+//     if (i % 2 === 0){
+//         console.log(num[i]);
+//     }
+//     i--;
+// }
+
+//6.
+// for(let i = num.length - 1; i >= 0; i--){
+//     if (i % 2 === 0){
+//         console.log(num[i]);
+//     }
+// }
+
+//7.
+// for(let i = num.length - 1; i >= 0; i--){
+//     if (i % 3 === 0){
+//         num[i] = 'okten';
+//     }
+// }
+// console.log(num);
+
