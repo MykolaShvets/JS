edf7ab622e83a42f7efa2af0b12cc76ef31c813c
MykolaShvets <m2009sv@gmail.com>
2021-11-30 20:29:34 +0200

    виправив hw

--- homework8/main.js
+++ homework8/main.js
@@ -2,19 +2,19 @@
 // -- Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
 // -- отримує текст з параграфа з id "content"
 
-let content = document.getElementById("content");
+let content = document.getElementById("content").innerText;
 
 // -- отримує текст з блоку з id "rules"
 
-let rules = document.getElementById('rules');
+let rules = document.getElementById('rules').innerText;
 
 // -- замініть текст параграфа з id 'content' на будь-який інший
 
-content.innerText = 'Helo okten!';
+document.getElementById("content").innerText = 'Helo okten!'; // саме запис content = 'Helo okten!'  не працює хоча в попередніх завданнях саме таке значення давав змінній.
 
 // -- замініть текст параграфа з id 'rules' на будь-який інший
 
-rules.innerText = 'This is HW from the 8-th lesson';
+document.getElementById('rules').innerText = 'This is HW from the 8-th lesson'; // саме запис rules = 'This is HW from the 8-th lesson' не працює хоча в попередніх завданнях саме таке значення давав змінній.
 
 // -- змініть кожному елементу колір фону на червоний
 // -- змініть кожному елементу колір тексту на синій
@@ -27,7 +27,7 @@ for (const element of everyElements.children) {
 
 // -- отримати весь список класів елемента з id=rules і вивести їх в console.log
 
-console.log(rules.className);
+console.log(document.getElementById('rules').className);
 
 // -- поміняти колір тексту у всіх елементів fc_rules на червоний
 
